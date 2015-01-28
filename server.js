7c39175ef24832cfd6d3b0b9c439164ee3489a09
Laura Qinzhi Li <laura.qz.li@gmail.com>
2015-01-28 16:59:50 +0000

    Add nick name route

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete Ninja now.');
 });
 
-app.listen(process.env.PORT || 4000);
+app.get('/nickname', function(request, response){
+    response.send('smiling-wombat');
+
+    
+});
+
+app.listen(
+    process.env.PORT || 4000,
+    process.env.IP||'local host'
+    );
